5f4c27228d82407ee707ef231f8b0fbca947b61e
Ryan Kerr <ryan@Ryans-MacBook-Air.local>
2025-10-21 19:47:53 +0300

    again

--- assets/jitsi.js
+++ assets/jitsi.js
@@ -1,19 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("/.netlify/functions/jitsi-token?room=rykerrmedicalmeeting")
-      .then(res => res.json())
-      .then(({ token }) => {
-        const cleanToken = token.replace(/(\r\n|\n|\r)/gm, "").trim();
+fetch("https://jitsi-for-rykerr-medical.netlify.app/.netlify/functions/jitsi-token?room=rykerrmedicalmeeting")
+  .then(res => res.json())
+  .then(({ token }) => {
+    const cleanToken = token.replace(/(\r\n|\n|\r)/gm, "").trim();
 
-        const options = {
-          roomName: "vpaas-magic-cookie-e515f4dfdbe24ae3a34c4247de2675db/rykerrmedicalmeeting",
-          parentNode: document.querySelector('#jaas-container'),
-          width: "100%",
-          height: 700,
-          jwt: cleanToken
-        };
+    const options = {
+      roomName: "vpaas-magic-cookie-e515f4dfdbe24ae3a34c4247de2675db/rykerrmedicalmeeting",
+      parentNode: document.querySelector('#jaas-container'),
+      width: "100%",
+      height: 700,
+      jwt: cleanToken
+    };
 
-        const domain = "8x8.vc";
-        const api = new JitsiMeetExternalAPI(domain, options);
-      })
-      .catch(err => console.error("❌ Could not load JWT:", err));
-});
+    const domain = "8x8.vc";
+    const api = new JitsiMeetExternalAPI(domain, options);
+  })
+  .catch(err => console.error("❌ Could not load JWT:", err));
